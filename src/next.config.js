fb63cde199e57126c45cbdcd0fd324e50af06811
Bratah123 <brandonnguyen3301@gmail.com>
2024-04-10 10:14:28 -0700

    FEAT: Add server actions and MAL cdn

--- src/next.config.js
+++ src/next.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.myanimelist.net',
+      }
+    ]
+  },
+  experimental: {
+    serverActions: true,
+  },
   rewrites: async () => {
     return [
       {
