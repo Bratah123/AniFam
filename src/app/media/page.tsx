d4020bc249f5be9c69171fed7e1910faab310e3d
Bratah123 <brandonnguyen3301@gmail.com>
2024-04-15 10:08:05 -0700

    FEAT: Add a constant to allow for flask server ip

--- src/app/media/page.tsx
+++ src/app/media/page.tsx
@@ -1,5 +1,11 @@
 import Navbar from '@/app/components/navbar';
 import { fetchAnyAvailSession } from '@/app/actions';
+import AnimePlayer from '@/app/components/anime_player';
+
+// THIS IS THE IP OF THE FLASK SERVER THAT SERVES THE VIDEOS
+// CHANGE THIS TO THE Public ipv4 address of the machine this is running on
+// if you want this web app to work in a public setting.
+const VIDEO_SERVER_IP = '127.0.0.1';
 
 export default async function MediaPage(params: any) {
 
@@ -11,6 +17,7 @@ export default async function MediaPage(params: any) {
         <div>
         <Navbar isAdmin={result.is_admin} onHome={false} onAdmin={false} onForums={false} />
         <h1>You are supposed to be watching: {animeName}</h1>
+        <AnimePlayer videoPath={`http://${VIDEO_SERVER_IP}:5328/static/E2.mp4`}/>
         </div>
     );
 }
