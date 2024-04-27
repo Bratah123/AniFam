0f049c45cca2bca7ae26e160fbdcdc0cdde910f9
Fynn <Tung02nguyen10@gmail.com>
2024-04-27 13:06:55 -0700

    CHORE: removing unnecessary null check

--- src/app/forums/page.tsx
+++ src/app/forums/page.tsx
@@ -6,8 +6,8 @@ import { TopicTable } from "@/app/components/topics_table";
 export default async function Forums(params: any) {
   const res = await fetchAnyAvailSession('forums', params.searchParams);
   return (
-    <div className="flex flex-col h-screen relative bg-slate-1000 bg-[url(/album_collage_1080.jpg)] bg-cover bg-center bg-no-repeat opacity-85">
-      <Navbar isAdmin={res?.is_admin} onHome={false} onForums={true} onAdmin={false} />
+    <div className="flex flex-col min-h-screen relative bg-slate-1000 bg-[url(/album_collage_1080.jpg)] bg-cover bg-center bg-no-repeat opacity-85">
+      <Navbar isAdmin={res.is_admin} onHome={false} onForums={true} onAdmin={false} />
       <div className="py-4 px-4 bg-black bg-opacity-80 relative">
         <h1 className="text-5xl font-bold text-white text-center mb-4">Ani x Family Forums</h1>
       </div>
