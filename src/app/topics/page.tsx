1eab3f2c6864ac489a095ad7dd6fa971654795e4
Fynn <Tung02nguyen10@gmail.com>
2024-04-27 13:06:20 -0700

    STYLE: remove topic button

--- src/app/topics/page.tsx
+++ src/app/topics/page.tsx
@@ -1,40 +1,27 @@
 import Navbar from '@/app/components/navbar';
 import { fetchAnyAvailSession } from '@/app/actions';
+import { CommentSection } from '@/app/components/comment_section';
 
-function initTopicButtonData(topic_id: string, topics: any[]) {
-    const topicButtons: any[] = [];
-    topics.forEach((topic, _) => {
-        topicButtons.push({
-            topic_id: topic_id,
-            title: topic.title,
-            long_description: topic.long_description,
-            short_description: topic.short_description,
-        });
-    });
-    return topicButtons;
-}
-export default async function TopicsPage(params: any) {
+
+export default async function TopicPage(params: any) {
     const searchParams = params.searchParams;
-    const res = await fetchAnyAvailSession('topics',{'topic_id': searchParams.topic_id});
-    const topics = res.topics;
-    const topic_id = searchParams.topic_id;
-    const topicButtonData = initTopicButtonData(topic_id, topics);
+
+    const res = await fetchAnyAvailSession('topicpage', {'title': searchParams.title});
+
     return (
-        <div className="flex flex-col h-screen relative bg-slate-1000 bg-[url(/album_collage_1080.jpg)] bg-cover bg-center bg-no-repeat opacity-85">
-            <Navbar isAdmin={res?.is_admin} onHome={false} onForums={true} onAdmin={false} />
+        <div>
+            <Navbar isAdmin={res.is_admin} onHome={false} onAdmin={false} onForums={false} />
+            <br></br>
+            <div className="flex flex-col h-screen relative bg-slate-1000 bg-[url(/album_collage_1080.jpg)] bg-cover bg-center bg-no-repeat opacity-85">
             <div className="py-4 px-4 bg-black bg-opacity-80 relative">
                 <h1 className="text-5xl font-bold text-white text-center mb-4">Ani x Family Topics</h1>
             </div>
             <div className="flex-grow relative">
-                <div className="grid grid-cols-1 gap-4 p-4">
-                    {topicButtonData.map((topic: any, index: number) => (
-                        <div key={index} className="bg-gray-950 p-4 rounded-lg shadow-lg">
-                            <h2 className="text-2xl font-bold">{topic.title}</h2>
-                            <p className="text-lg">{topic.short_description}</p>
-                        </div>
-                    ))}
+                <div className="grid grid-cols-1 gap-4 p-4">  
                 </div>
             </div>
+        </div>
+        <CommentSection/>
         </div>
     );
 }
