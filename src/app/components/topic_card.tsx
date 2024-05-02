ce6204c2d2722a0a73f88eb29612127832b6931d
Fynn <Tung02nguyen10@gmail.com>
2024-05-02 00:21:40 -0700

    CHORE: add short description for logging

--- src/app/components/topic_card.tsx
+++ src/app/components/topic_card.tsx
@@ -5,7 +5,6 @@ import { TopicToolTipProps } from '@/app/components/topic_card_tool_tip';
 interface TopicCardProp extends TopicToolTipProps {
   topic_id: number;
   title: string;
-  long_description: string;
   short_description: string;
 }
 
@@ -16,18 +15,18 @@ export default function TopicCard({
   return (
     <div className="relative w-full h-full p-6 bg-cyan-500 bg-opacity-80 rounded-lg shadow-md hover:shadow-lg transition-opacity duration-300">
       <Link
-        href={{
-          pathname: '/topics',
-          query: { topic_title: title },
-        }}
-        passHref
-      >
-        <div className="flex flex-col items-center justify-center text-center hover:cursor-pointer hover:bg-cyan-700 hover:bg-opacity-80 w-full h-full p-4">
-          <h2 className="text-xl font-bold text-white">{title}</h2>
-          <div className="my-2 w-full border-b border-black"></div>
-          <p className="text-sm text-white">{short_description}</p>
+            href={{
+              pathname: '/topics',
+              query: { topic_title: title, short_description },
+            }}
+            passHref
+          >
+            <div className="flex flex-col items-center justify-center text-center hover:cursor-pointer hover:bg-cyan-700 hover:bg-opacity-80 w-full h-full p-4">
+              <h2 className="text-xl font-bold text-white">{title}</h2>
+              <div className="my-2 w-full border-b border-black"></div>
+              <p className="text-sm text-white">{short_description}</p>
+            </div>
+          </Link>
         </div>
-      </Link>
-    </div>
   );
 }
