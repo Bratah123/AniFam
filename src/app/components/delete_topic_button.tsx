8a063600c4951e4659ce67917732c41718c5bac7
Fynn <Tung02nguyen10@gmail.com>
2024-05-05 22:45:23 -0700

    FEAT: add topic delete button

---
+++ src/app/components/delete_topic_button.tsx
@@ -0,0 +1,28 @@
+'use client';
+import React from "react";
+import { useRouter } from "next/navigation";
+
+type ButtonProp = {
+    text: string;
+    link: string;
+};
+export default function DeleteLinkButton({ text, link }: ButtonProp) {
+    const router = useRouter();
+    return (
+        <div style={{
+            position: 'fixed',  
+            top: 0,             
+            right: 0,          
+            zIndex: 1000      
+        }}>
+            <button
+                onClick={() => router.push(link)}
+                className="rounded bg-indigo-700 px-8 py-2 text-sm text-white transition duration-150 ease-in-out
+                hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-offset-2">
+                {text}
+            </button>
+        </div>
+    );
+}
+
+
