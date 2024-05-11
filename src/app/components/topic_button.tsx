1ecfde8f1516a0ce3255f8754c7e3a2de7c220ff
Fynn <Tung02nguyen10@gmail.com>
2024-05-11 10:00:47 -0700

    NIT: remove reference comments

--- src/app/components/topic_button.tsx
+++ src/app/components/topic_button.tsx
@@ -19,8 +19,8 @@ export default function TopicButton(prop: ButtonProp) {
     };
 
     return (
-        <div className="flex justify-center items-center w-full pt-1"> {/* Full-screen with padding */}
-            <div className="w-[97%] p-4 bg-cyan-700 text-black rounded-lg shadow-md hover:shadow-lg transition-opacity duration-300 flex justify-center items-center"> {/* Centered button container */}
+        <div className="flex justify-center items-center w-full pt-1"> 
+            <div className="w-[97%] p-4 bg-cyan-700 text-black rounded-lg shadow-md hover:shadow-lg transition-opacity duration-300 flex justify-center items-center"> 
                 <button
                     onClick={handleClick}
                     className={`w-full h-full bg-cyan-500 bg-opacity-80 text-white text-9xl font-semibold rounded-lg transition-opacity duration-300 focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:ring-opacity-50 hover:bg-cyan-600 relative overflow-hidden ${expand ? 'expand' : ''}`}
