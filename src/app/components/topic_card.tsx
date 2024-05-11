6e2ad12b4b7e1dcac5af42056372b7f70fe1aac3
Fynn <Tung02nguyen10@gmail.com>
2024-05-11 01:54:45 -0700

    STYLE: add little anya to topic car

--- src/app/components/topic_card.tsx
+++ src/app/components/topic_card.tsx
@@ -1,5 +1,6 @@
 'use client'
 import Link from 'next/link';
+import Image from 'next/image'; 
 import { TopicToolTipProps } from '@/app/components/topic_card_tool_tip';
 
 interface TopicCardProp extends TopicToolTipProps {
@@ -15,23 +16,34 @@ export default function TopicCard({
   user,
 }: TopicCardProp) {
   return (
-    <div className="relative w-full h-full p-6 bg-cyan-500 bg-opacity-80 rounded-lg shadow-md hover:shadow-lg transition-opacity duration-300">
-      <div className="user-box absolute top-0 right-0 p-2 bg-blue-500 text-gray-900 rounded-lg">
-        Posted by: {user}
-      </div>
-      <Link
-        href={{
-          pathname: '/topics',
-          query: { topic_title: title, short_description, user },
-        }}
-        passHref
-      >
-        <div className="flex flex-col items-start justify-start text-left hover:cursor-pointer hover:bg-cyan-700 hover:bg-opacity-80 w-full h-full p-4">
-          <h2 className="text-xl font-bold text-white">{title}</h2>
-          <div className="my-2 w-full border-b border-black"></div>
-          <p className="text-sm text-white">{short_description}</p>
+    <div className="relative w-full h-full bg-cyan-500 bg-opacity-80 rounded-lg shadow-md hover:shadow-lg transition-opacity duration-300">
+      <div className="p-6">
+        <div className="absolute top-0 left-0">
+          <Image
+            src="/anya_chibi_t.png"
+            alt="Anya chibi image"
+            width={25} 
+            height={25} 
+            className="rounded-lg"
+          />
         </div>
-      </Link>
+        <div className="user-box absolute top-0 right-0 p-2 bg-blue-500 text-gray-900 rounded-lg">
+          Posted by: {user}
+        </div>
+        <Link
+          href={{
+            pathname: '/topics',
+            query: { topic_title: title, short_description, user },
+          }}
+          passHref
+        >
+          <div className="flex flex-col items-start justify-start text-left hover:cursor-pointer hover:bg-cyan-700 hover:bg-opacity-80 w-full h-full">
+            <h2 className="text-xl font-bold text-white">{title}</h2>
+            <div className="my-2 w-full border-b border-black"></div>
+            <p className="text-sm text-white">{short_description}</p>
+          </div>
+        </Link>
+      </div>
     </div>
   );
 }
